6406883935cd7ce1adf83c84dfc63304ede47b49
AntonyIS <antonyshikubu@mail.com>
2019-11-26 07:36:42 +0300

    Better error handling and connection to the database

--- routes/routes.js
+++ routes/routes.js
@@ -7,132 +7,95 @@ app.get('/', function(req, res){
 
 // /api/quotes :RETRIEVE
 app.get('/api/quotes', function(req, res){
-	db.connect(function(err){
-		if(err)
+	 let sql = "SELECT * FROM `quotes`";
+	db.query(sql, function(error, results, fields){
+		if(error)
 		{
-			console.log("Somethig went wrong with")
+			throw error;
 		}
-
-	
-
-		let sql = "SELECT * FROM `quotes`";
-
-		db.query(sql, function(err, results){
-			if(err)
-			{
-				throw err;
-			}
-			else{
-				res.send(results);
-			}
-		});
-	
-		
+		res.send(JSON.stringify({"status": 200, "error": null,"response":results}));
 	});
+	
 });
 // /api/quotes/ID :RETRIEVE
 app.get('/api/quotes/:id/', function(req, res){
-	let quoteId = req.params.id
-	db.connect(function(err){
-		if(err)
-		{
-			console.log("Somethig went wrong with")
-		}
-		let sql = "SELECT `id`, `author`, `quote`, `date` FROM `quotes` WHERE id='"+quoteId+" '";
+	let quoteId = req.params.id;
+	
+	let sql = "SELECT `id`, `author`, `quote`, `date` FROM `quotes` WHERE id='"+quoteId+" '";
 
-		db.query(sql, function(err, result){
-			if(err)
-			{
-				throw err;
-			}
-			else
-			{
-				res.send(result);
-			}
-		});
+	db.query(sql, function(error, result){
+		if(error)
+		{
+			throw error;
+		}
+		else
+		{
+			res.send(result);
+		}
 	});
+	
 
 });
 
 // /api/quotes/ :CREATE
 app.post('/api/quotes/', function(req, res){
-	db.connect(function(err){
-		if(err)
-		{
-			console.log("Somethig went wrong with")
-		}
-
-		
-		let author = req.body.author;
-		let quote = req.body.quote;
-		
-
-		let sql = "INSERT INTO `quotes`(`id`, `author`, `quote`, `date`) VALUES (NULL,' "+ author +" ',' "+ quote +"','"+ new Date() +" ')";
-
-		db.query(sql, function(err, result){
-			if(err)
-			{
-				throw err;
-			}
-			else{
-				res.send("Data added successfuly");
-			}
-		});
 	
 		
+	let author = req.body.author;
+	let quote = req.body.quote;
+	
+
+	let sql = "INSERT INTO `quotes`(`id`, `author`, `quote`, `date`) VALUES (NULL,' "+ author +" ',' "+ quote +"','"+ new Date() +" ')";
+
+	db.query(sql, function(error, result){
+		if(error)
+		{
+			throw error;
+		}
+		else{
+			res.send(JSON.stringify({"status": 200, "error": null,"response":result}));
+		}
 	});
 	
 	
 });
-// /api/quotes/id :UPDATE
 
 app.put('/api/quotes/:id/', function(req, res){
-	let quoteId = req.params.id
-	db.connect(function(err){
-		if(err)
-		{
-			console.log("Somethig went wrong with")
-		}
-		let author = req.body.author;
-		let quote = req.body.quote;
-		
-		let sql = "UPDATE `quotes` SET `author`=' "+author+" ',`quote`=' "+quote+" ' WHERE id=' "+quoteId+" '";
+	let quoteId = req.params.id;
+	
+	let author = req.body.author;
+	let quote = req.body.quote;
+	
+	let sql = "UPDATE `quotes` SET `author`=' "+author+" ',`quote`=' "+quote+" ' WHERE id=' "+quoteId+" '";
 
-		db.query(sql, function(err, result){
-			if(err)
-			{
-				throw err;
-			}
-			else
-			{
-				res.send(result);
-			}
-		});
+	db.query(sql, function(error, result){
+		if(error)
+		{
+			throw error;
+		}
+		else
+		{
+			res.send(result);
+		}
 	});
 
 });
-// /api/quotes/ :DELETE
-app.delete('/api/quotes/:id/', function(req, res){
-	let quoteId = req.params.id
-	db.connect(function(err){
-		if(err)
-		{
-			console.log("Somethig went wrong with")
-		}
-		
-		
-		let sql = "DELETE FROM `quotes` WHERE id=' "+quoteId+" '";
 
-		db.query(sql, function(err, result){
-			if(err)
-			{
-				throw err;
-			}
-			else
-			{
-				res.send("Record delete successfuly");
-			}
-		});
+app.delete('/api/quotes/:id/', function(req, res){
+	let quoteId = req.params.id;
+	
+		
+	let sql = "DELETE FROM `quotes` WHERE id=' "+quoteId+" '";
+
+	db.query(sql, function(error, result){
+		if(error)
+		{
+			throw error;
+		}
+		else
+		{
+			res.send("Record delete successfuly");
+		}
 	});
 
 });
--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const db = require('./dbconfig/db');//configuration to MYSQL database
 
 app.use(bodyParser.json());//Only json request used at the moment
 
+app.use(function(err, req, res,next){
+	console.log(error(err.stack));
+	res.status(500).send("Something went wrong")
+});//Error Handling
+db.connect(); 
 routes(app, db);
+
+
+
 
 // Server listening on port 3000
 app.listen(port,function(){
